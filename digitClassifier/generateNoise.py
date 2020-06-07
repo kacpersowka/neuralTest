15653d078eafd03076b61ca2722878ec71009179
sowkak <sowkak@coventry.ac.uk>
2020-06-07 17:06:49 +0100

    Genearate noise

---
+++ digitClassifier/generateNoise.py
@@ -0,0 +1,30 @@
+#add noise
+import pickle,random
+x=[]
+y=[]
+if len(sys.argv)>1:
+    na=int(sys.argv[1])
+else:
+    na=10000
+    
+if len(sys.argv)>1:
+    nb=int(sys.argv[1])
+else:
+    nb=10000
+
+for i in range(na):
+    x.append([random.random() for j in range(28**2)])
+    y.append([0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.99])
+    
+with open("noise.pkl", "bw") as fh:
+    data = (x,y)
+    pickle.dump(data, fh)
+x=[]
+y=[]
+for i in range(nb):
+    x.append([random.random() for j in range(28**2)])
+    y.append([0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.99])
+    
+with open("noiseTest.pkl", "bw") as fh:
+    data = (x,y)
+    pickle.dump(data, fh)
