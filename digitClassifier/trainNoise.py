b8395e6150d47a08a2ed77632bef04ebdfbd7fc3
sowkak <sowkak@coventry.ac.uk>
2020-06-07 17:01:38 +0100

    Noise sensitive

---
+++ digitClassifier/trainNoise.py
@@ -0,0 +1,66 @@
+from neural import *
+import sys,time
+import pickle,random
+
+with open("pickled_mnist.pkl", "br") as fh:
+    data = pickle.load(fh)
+
+train_imgs = data[0]
+test_imgs = data[1]
+train_labels = data[2]
+test_labels = data[3]
+train_labels_one_hot = data[4]
+test_labels_one_hot = data[5]
+x=train_imgs
+y=train_labels_one_hot
+for i in y:
+    i.append(0.01)
+image_size = 28 # width and length
+no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
+image_pixels = image_size * image_size
+
+with open("noise.pkl", "br") as fh:
+    data = pickle.load(fh)
+x+=data[0]
+y+=data[1]
+    
+#train n e m bs lf l
+#train e bs lf l n m
+if len(sys.argv)>1:
+    e=float(sys.argv[1])
+else:
+    e=0.1
+if len(sys.argv)>2:
+    bs=int(sys.argv[2])
+else:
+    bs=5
+if len(sys.argv)>3:
+    lf=float(sys.argv[3])
+else:
+    lf=1.001
+if len(sys.argv)>4:
+    l=int(sys.argv[4])
+else:
+    l=None 
+if len(sys.argv)>5:
+    n=int(sys.argv[5])
+else:
+    n=1
+if len(sys.argv)>6:
+    m=float(sys.argv[6])
+else:
+    m=0.8
+if len(sys.argv)>7:
+    wf=int(sys.argv[7])
+else:
+    wf=1
+if len(sys.argv)>8:
+    bf=int(sys.argv[8])
+else:
+    bf=1
+
+random.seed(time.time())
+w,b=sgd(x[:l],y[:l],[8,8,11],n,e,m,wf,bf,bs,lf,functions=function1,lossFunction=[crossEntropy,crossEntropyGradient],functionDerivatives=function1Derivative,functArguments=[[[rectLinear],[softmax]],[[rectLinearDerivative],[softmaxDerivative]]])
+with open("trained.pkl", "bw") as fh:
+    data = (w,b)
+    pickle.dump(data, fh)
---
+++ digitClassifier/validateNoise.py
@@ -0,0 +1,54 @@
+from neural import *
+import sys,time
+import pickle
+import matplotlib.pyplot as plt
+
+with open("pickled_mnist.pkl", "br") as fh:
+    data = pickle.load(fh)
+
+train_imgs = data[0]
+test_imgs = data[1]
+train_labels = data[2]
+test_labels = data[3]
+train_labels_one_hot = data[4]
+test_labels_one_hot = data[5]
+image_size = 28 # width and length
+no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
+image_pixels = image_size * image_size
+
+if len(sys.argv)>1:
+    s=int(sys.argv[1])
+else:
+    s=True
+if s:
+    x=test_imgs
+    y=test_labels_one_hot
+else:
+    x=train_imgs
+    y=train_labels_one_hot
+
+if len(sys.argv)>2:
+    n=int(sys.argv[2])
+else:
+    n=-1
+    
+if len(sys.argv)>3:
+    offset=int(sys.argv[3])
+else:
+    offset=0
+
+if len(sys.argv)>4:
+    r=int(sys.argv[4])
+else:
+    r=3   
+    
+if n==-1:
+    n=len(x)
+    
+with open("trained.pkl", "br") as fh:
+    w,b = pickle.load(fh)
+
+for i in range(offset,min(n+offset,len(x))):
+    a=feedForward(x[i],w,b,y[i],function1,crossEntropy,[[expRectLinear],[softmax]])
+    print('Input: ',i,'\nOutput: ',numpy.round(a[-2],r),'\nTarget:',numpy.round(y[i],r),'\nError:',numpy.round(a[-1],r),'\nCorrect:',int(a[-2].index(max(a[-2]))==y[i].index(max(y[i]))))    
+    
