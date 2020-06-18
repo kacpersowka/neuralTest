e71d53fbe35c0e799ae44d10ed04c8db81c84904
sowkak <sowkak@coventry.ac.uk>
2020-06-18 13:09:13 +0100

    Feedforward CNN fix

--- digitClassifier/data.py
+++ digitClassifier/data.py
@@ -3,21 +3,24 @@ import sys,time
 import pickle
 import matplotlib.pyplot as plt
 
-with open("pickled_mnist.pkl", "br") as fh:
+with open("miniMNIST.pkl", "br") as fh:
     data = pickle.load(fh)
 
-train_imgs = data[0]
-test_imgs = data[1]
-train_labels = data[2]
-test_labels = data[3]
-train_labels_one_hot = data[4]
-test_labels_one_hot = data[5]
+#train_imgs = data[0]
+#test_imgs = data[1]
+#train_labels = data[2]
+#test_labels = data[3]
+#train_labels_one_hot = data[4]
+#test_labels_one_hot = data[5]
+
+x=data[0]
+y=data[1]
 
 image_size = 28 # width and length
 no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
 image_pixels = image_size * image_size
 
-def drawFigure(index,dataset=train_imgs,fname='test.png'):
+def drawFigure(fig,fname='test.png'):
     f=plt.figure()
-    plt.imshow(dataset[index].reshape((28,28)), cmap="Greys")
+    plt.imshow(fig, cmap="Greys")
     f.savefig(fname)
