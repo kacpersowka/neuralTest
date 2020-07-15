7ea1786ddb7cda6b9bd571d003cf42244abf76d0
sowkak <sowkak@coventry.ac.uk>
2020-07-15 17:45:10 +0100

    autoencoder

--- digitClassifier/autoencoder.py
+++ digitClassifier/autoencoder.py
@@ -19,33 +19,27 @@ image_size = 28 # width and length
 no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
 image_pixels = image_size * image_size
 
-if len(sys.argv)>1:
-    n=int(sys.argv[1])
-else:
-    n=1
-if len(sys.argv)>2:
-    e=float(sys.argv[2])
-else:
-    e=0.1
-if len(sys.argv)>3:
-    m=float(sys.argv[3])
-else:
-    m=0.8   
-if len(sys.argv)>4:
-    l=int(sys.argv[4])
-else:
-    l=None
-if len(sys.argv)>5:
-    wf=int(sys.argv[5])
-else:
-    wf=1
-if len(sys.argv)>6:
-    bf=int(sys.argv[6])
-else:
-    bf=1
+if __name__=="__main__":
 
-random.seed(time.time())
-w,b=train(x[:l],x[:l],[64,image_size**2],n,e,m,wf,bf,functions=function1,lossFunction=[mse,mseGradient],functionDerivatives=function1Derivative,functArguments=[[[expRectLinear],[expRectLinear]],[[expRectLinearDerivative],[expRectLinearDerivative]]])
-with open("trained.pkl", "bw") as fh:
-    data = (w,b)
-    pickle.dump(data, fh)
+    if len(sys.argv)>1:
+        n=int(sys.argv[1])
+    else:
+        n=1
+    if len(sys.argv)>2:
+        e=float(sys.argv[2])
+    else:
+        e=0.1
+    if len(sys.argv)>3:
+        m=float(sys.argv[3])
+    else:
+        m=0.8   
+    if len(sys.argv)>4:
+        l=int(sys.argv[4])
+    else:
+        l=None
+
+    random.seed(time.time())
+    w,b=train(x[:l],x[:l],[128,image_size**2],n,e,m,functions=function1,lossFunction=[mse,mseGradient],functionDerivatives=function1Derivative,functArguments=[[[expRectLinear],[expRectLinear]],[[expRectLinearDerivative],[expRectLinearDerivative]]])
+    with open("trained.pkl", "bw") as fh:
+        data = (w,b)
+        pickle.dump(data, fh)
